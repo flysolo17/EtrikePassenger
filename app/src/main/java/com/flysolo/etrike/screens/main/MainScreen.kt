0a6d2fea00335bfbc81e4cc5eab8e1daa7c7f728
flysolo17 <jmballangca002@gmail.com>
2025-04-23 17:30:48 -0700

    passenger text at topbar

--- app/src/main/java/com/flysolo/etrike/screens/main/MainScreen.kt
+++ app/src/main/java/com/flysolo/etrike/screens/main/MainScreen.kt
@@ -6,10 +6,14 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.location.LocationManager
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Send
 import androidx.compose.material.icons.rounded.Message
@@ -34,6 +38,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -146,14 +151,20 @@ fun MainScreen(
                             },
                             title = {
 //                                Text("${state.user?.location?.latitude} - ${state.user?.location?.longitude}")
-                                Box(
+                                Row (
                                     modifier = modifier.fillMaxWidth(),
-                                    contentAlignment = Alignment.Center
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    horizontalArrangement = Arrangement.Center
                                 ) {
+
                                     Image(
                                         painter = painterResource(R.drawable.top_etrike),
                                         contentDescription = "Top Bar"
                                     )
+                                    Spacer(
+                                        modifier = modifier.width(8.dp)
+                                    )
+                                    Text("PASSENGER", style = MaterialTheme.typography.titleSmall)
                                 }
                             },
                             actions = {
